13da31c16ae541b7754ff9c4437296e0ca0a1cce
Ryo Yanagida <Ryo.Yanagida@glasgow.ac.uk>
2025-04-29 18:13:57 +0100

    escape quotes

--- cleanup_aff_map.py
+++ cleanup_aff_map.py
@@ -8,9 +8,10 @@ from pathlib import Path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import ietfdata
-import ietfdata.tools.affiliations as aff
+import ietfdata.tools.affiliations as af
 import ietfdata.tools.affiliation_mapping_dictionary as afmap
 if __name__ == '__main__':
+    aff = af.Affiliations()
     output_path = None
     input_path = None
     if len(sys.argv) == 2:
@@ -21,9 +22,9 @@ if __name__ == '__main__':
     new_map = dict()
     for key in keys:
         tmp_key = key
-        tmp_key = aff.cleanup_affiliation_academic(tmp_key)
-        tmp_key = aff.cleanup_affiliation_strip_chars(tmp_key)
-        tmp_key = aff.cleanup_affiliation_suffix(tmp_key)
+        tmp_key = aff._cleanup_affiliation_academic(tmp_key)
+        tmp_key = aff._cleanup_affiliation_strip_chars(tmp_key)
+        tmp_key = aff._cleanup_affiliation_suffix(tmp_key)
         if key in new_map:
             continue
         key_lower_found = False
@@ -41,21 +42,23 @@ if __name__ == '__main__':
                 continue
             tmp_list = list()
             for tmp_item in tmp_val:
-                tmp_item = aff.cleanup_affiliation_strip_chars(tmp_item) # remove white spaces
-                tmp_item = aff.cleanup_affiliation_suffix(tmp_item) # remove suffixes
+                tmp_item = aff._cleanup_affiliation_strip_chars(tmp_item) # remove white spaces
+                tmp_item = aff._cleanup_affiliation_suffix(tmp_item) # remove suffixes
                 if tmp_item == "":
                     continue
-                tmp_list.append(tmp_item)
-            new_map[tmp_key] = tmp_list 
+                if tmp_item != tmp_key: # purge A->A mapping
+                    tmp_list.append(tmp_item)
+            if len(tmp_list) > 0:
+                new_map[tmp_key] = tmp_list 
         
     
     with open(output_path,'w') as f:
         print("affiliation_list_map = {",file=f)
         for aff in new_map:
-            print(f'    "{aff}":\n        [',end='',file=f)
+            print(f'    "{aff.replace('"','\\"')}":\n        [',end='',file=f)
             list_str = ""
             for val in new_map[aff]:
-                list_str+=(f'"{val}",')
+                list_str+=(f'"{val.replace('"','\\"')}",')
             list_str = list_str.rstrip(',')
             print(f"{list_str}],",file=f)
         print("}",file=f)
